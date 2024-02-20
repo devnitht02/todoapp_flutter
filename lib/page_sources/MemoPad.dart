3a4319a7e06eedd8b02f025c7c8e9dbe99175bc6
Devnith Tissera <devnithtissera@gmail.com>
2024-02-20 11:17:17 +0530

    memopad feature

---
+++ lib/page_sources/MemoPad.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_summernote/flutter_summernote.dart';
+import 'package:todo_go/main.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NotesPage(title: 'Memo Pad'),
+    );
+  }
+}
+
+class NotesPage extends StatefulWidget {
+  NotesPage({required this.title});
+
+  final String title;
+
+  @override
+  _NotesPageState createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  GlobalKey<FlutterSummernoteState> _keyEditor = GlobalKey();
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Center(
+          child: Text(widget.title,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+              fontFamily: 'Roboto-Black',
+
+            ),),
+        ),
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () async {
+              final value = (await _keyEditor.currentState?.getText());
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                duration: Duration(seconds: 5),
+                content: Text(value ?? '-'),
+              ));
+            },
+          )
+        ],
+      ),
+      backgroundColor: Colors.teal,
+      body: FlutterSummernote(
+        hint: 'Your text here...',
+        key: _keyEditor,
+        hasAttachment: true,
+        customToolbar: """
+          [
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['insert', ['link', 'table', 'hr']]
+          ]
+        """,
+      ),
+    );
+  }
+}
